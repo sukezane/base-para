a7b9e55961cfd9c3f2eaa1b232abf1223e0f0e2b
Yuma Yamada <kommunika1029@gmail.com>
2019-06-27 15:08:07 +0900

    update

--- js/vue/news.js
+++ js/vue/news.js
@@ -4,16 +4,26 @@ import Axios from "axios";
 new Vue({
     el: "#news",
     data: {
-        post: []
+        posts: []
     },
     created() {
         this.fetch();
+    },
+    computed: {
+        formatDate(dateGmt) {
+            const date = new Date(dateGmt);
+            const month =
+              date.getMonth() < 9
+                  ? "0" + (date.getMonth() + 1)
+                  : date.getMonth() + 1;
+            return date.getFullYear() + '.' + month + '.' + date.getDate();
+        }
     },
     methods: {
         fetch() {
             Axios.get('https://mag.digle.tokyo/wp-json/wp/v2/posts?category=1&per_page=3').then(res => {
                 console.log(res);
-                this.post = this.post.concat(res.data);
+                this.posts = this.posts.concat(res.data);
             });
         }
     }
