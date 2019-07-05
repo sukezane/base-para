155e14dd01acd3acb9d2eced15ce7af1f83264f3
Yuma Yamada <kommunika1029@gmail.com>
2019-07-05 15:47:13 +0900

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-new Swiper('#swiper-top', {
-    slidesPerView: 1,
-    centeredSlides: true,
-    loop: true,
-    effect: 'fade',
-    autoplay: {
-        delay: 5000,
-    },
-    paginationClickable: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    }
-});
-
 $('.sw-Menu_Button').on('click', function(){
     $('.sw-Menu_ListWrapper').toggleClass('sw-Menu_ListWrapper-active');
     $('body').toggleClass('is-menu');
