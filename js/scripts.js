ad95636e8a31310536de50087072056ff019c3d3
Yuma Yamada <kommunika1029@gmail.com>
2019-07-11 13:37:27 +0900

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$('.st-Header_MenuButton').on('click', function(){
+$('.st-Header_MenuButton').on('click', function() {
     $('.st-Header_MenuListWrapper').toggleClass('st-Header_MenuListWrapper-active');
+});
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+        $('.st-Header-index').addClass('.st-Header-active');
+    } else {
+        $('.st-Header-index').removeClass('.st-Header-active');
+    }
 });
