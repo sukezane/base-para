3602c6c9d4c57f02a306069c570c64637e523cb5
Yuma Yamada <kommunika1029@gmail.com>
2019-09-17 19:27:31 +0900

    commit

--- js/vue/router/index.js
+++ js/vue/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue"
 import Router from "vue-router"
 
+import Home from "../components/IndexPage.vue"
+import About from "../components/AboutPage.vue"
+import Category from "../components/CategoryPage.vue"
+
+
 Vue.use(Router)
 
 const router = new Router({
@@ -18,7 +23,7 @@ const router = new Router({
       {
         path: "/:categoryId",
         name: "Category",
-        component: Cateogory
+        component: Category
       }
     ],
     mode: "history",
