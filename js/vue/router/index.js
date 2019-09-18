74b61f7b17e4316fa772e626ebe094a5f9d0cda1
Yuma Yamada <kommunika1029@gmail.com>
2019-09-18 11:23:12 +0900

    commit

--- js/vue/router/index.js
+++ js/vue/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 
-import Home from "../components/IndexPage.vue"
+import home from "../components/IndexPage.vue"
 import About from "../components/AboutPage.vue"
 import Category from "../components/CategoryPage.vue"
 
@@ -12,8 +12,8 @@ const router = new Router({
     routes: [
       {
         path: "/",
-        name: "Home",
-        component: About
+        name: "home",
+        component: home
       },
       {
         path: "/about",
