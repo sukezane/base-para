2b0c02db5fb7b00ad14c63481b165314ea22a4dd
Yuma Yamada <kommunika1029@gmail.com>
2019-09-20 16:42:36 +0900

    commit

--- js/vue/components/CategoryPage.vue
+++ js/vue/components/CategoryPage.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="sw-Product is-sticky">
         <h2 class="sw-Product_Headline">{{ $route.params.categoryName }}</h2>
-         <div class="clearfix" v-html="$route.params.categoryItems">
+         <div class="clearfix" v-html="this.catgory.items">
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            category: {
+                items: null
+            }
+        }
+    },
+    methods: {
+        async fetchCategory() {
+            const res = await Axios.get("https://paragoods.thebase.in/load_items/categories/"+ this.$route.params.categoryId +"/1/user_paragoods_5d0c5a11862e1/0",{
+                dataType: 'html'
+            });
+            this.category.items = res.data;
+        }
+    }
 }
 </script>
