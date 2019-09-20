d5661382b7e939fafe1bded97a35ea90f3589937
Yuma Yamada <kommunika1029@gmail.com>
2019-09-20 16:00:13 +0900

    commit

--- js/vue/app.js
+++ js/vue/app.js
@@ -1,12 +1,49 @@
 import Vue from "Vue"
-// import VueRouter from "vue-router"
+import VueRouter from "vue-router"
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-// Vue.use(VueRouter)
+Vue.use(VueRouter)
 Vue.use(VueAwesomeSwiper)
 
-import router from "./router/index"
+
 import App from "./App.vue"
+import Home from "./components/IndexPage.vue"
+import About from "./components/AboutPage.vue"
+import Category from "./components/CategoryPage.vue"
+
+
+const router = new VueRouter({
+    routes: [
+      {
+        path: "/",
+        name: "Home",
+        component: Home
+      },
+      {
+        path: "/about",
+        name: "About",
+        component: About
+      },
+      {
+        path: "/categories/:categoryId",
+        name: "Category",
+        component: Category
+      }
+    ],
+    mode: "history",
+    // base: "",
+
+    // Prevents window from scrolling back to top
+    // when navigating between components/views
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return { x: 0, y: 0 }
+      }
+    }
+  })
+
 
 new Vue({
     el: "#app",
